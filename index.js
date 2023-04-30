60d92a44213b3007308c1ca475944e7302c115c9
Michael Douglas <michael.sousa@remsoft.com.br>
2023-04-30 00:38:15 -0300

    #1

---
+++ index.js
@@ -0,0 +1,105 @@
+const wppconnect = require('@wppconnect-team/wppconnect');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+let newClient = null;
+
+wppconnect.create({
+    session: 'sessionName', //Pass the name of the client you want to start the bot
+    catchQR: (base64Qrimg, asciiQR, attempts, urlCode) => {
+      console.log('Number of attempts to read the qrcode: ', attempts);
+      console.log('Terminal qrcode: ', asciiQR);
+      console.log('base64 image string qrcode: ', base64Qrimg);
+      console.log('urlCode (data-ref): ', urlCode);
+    },
+    statusFind: (statusSession, session) => {
+      console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
+      //Create session wss return "serverClose" case server for close
+      console.log('Session name: ', session);
+    },
+    onLoadingScreen: (percent, message) => {
+      console.log('LOADING_SCREEN', percent, message);
+    },
+    headless: true, // Headless chrome
+    devtools: false, // Open devtools by default
+    useChrome: true, // If false will use Chromium instance
+    debug: false, // Opens a debug session
+    logQR: true, // Logs QR automatically in terminal
+    browserWS: '', // If u want to use browserWSEndpoint
+    browserArgs: [''], // Parameters to be added into the chrome browser instance
+    puppeteerOptions: {}, // Will be passed to puppeteer.launch
+    disableWelcome: false, // Option to disable the welcoming message which appears in the beginning
+    updatesLog: true, // Logs info updates automatically in terminal
+    autoClose: 60000, // Automatically closes the wppconnect only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+    tokenStore: 'file', // Define how work with tokens, that can be a custom interface
+    folderNameToken: './tokens', //folder name when saving tokens
+    // BrowserSessionToken
+    // To receive the client's token use the function await clinet.getSessionTokenBrowser()
+    sessionToken: {
+      WABrowserId: '"UnXjH....."',
+      WASecretBundle: '{"key":"+i/nRgWJ....","encKey":"kGdMR5t....","macKey":"+i/nRgW...."}',
+      WAToken1: '"0i8...."',
+      WAToken2: '"1@lPpzwC...."',
+    }
+  })
+  .then((client) => start(client))
+  .catch((error) => console.log(error));
+
+  function start(client) {
+    newClient = client;
+
+    /* client.onMessage((message) => {
+      if (message.body === 'Hello') {
+        client
+          .sendText(message.from, 'Hello, how I may help you?')
+          .then((result) => {
+            console.log('Result: ', result); //return object success
+          })
+          .catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+          });
+      }
+    }); */
+
+  }
+
+  async function sedMessage(phone, text, res){
+
+    await newClient
+    .sendText(`${phone}@c.us`, text)
+    .then((result) => {
+        
+        res.status(200).json({
+            message: 'Mensagem enviada com sucesso',
+            success: true,
+          });
+          
+    })
+    .catch((erro) => {
+        console.error('Error when sending: ', erro); //return object error
+
+        res.status(200).json({
+            message: 'Falha ao enviar mensagem',
+            success: false,
+          });
+    });
+
+  }
+  
+  app.use(bodyParser.json());
+  
+  app.post('/send', (req, res) => {
+    const phone = req.body.phone;
+    const text = req.body.text;
+  
+    sedMessage(phone, text, res);
+    // Aqui você pode colocar a lógica para enviar a mensagem para o número de telefone
+  
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+  
